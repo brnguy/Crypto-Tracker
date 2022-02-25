a808191a22f5a43c6d7a5f741dab44ae8b47dd48
brnguy <nguyen.bryan15@gmail.com>
2022-02-25 09:19:32 -0800

    Added files

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const ejsLayouts = require('express-ejs-layouts')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.set('view engine', 'ejs')
+app.use(ejsLayouts)
+
+app.get('/', (req, res) => {
+    res.send('Hello')
+})
+
+app.listen(PORT, function() {
+    console.log('SERVER IS LIVE')
+} )
