e1a0067bd446701ef7d664fcdea8b40762e0e533
brnguy <nguyen.bryan15@gmail.com>
2022-03-01 16:36:03 -0800

    Created Login Page

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,8 +11,53 @@ router.get('/new', (req, res) => {
     res.render('./user/new')
 })
 
+router.post('/', async (req, res) => {
+    const [newUser, created] = await db.user.findOrCreate({
+        where: {email: req.body.email}
+    })
+    if(!created){
+        console.log('User already exists')
+        // render the login page and send an appropriate message
+    } else {
+        // hash the user
+        const hashedPassword = bcrypt.hashSync(req.body.password, 10)
+        newUser.password = hashedPassword
+        await newUser.save()
+
+        // encrypt the user id via AES
+        const encryptedUserId = cryptojs.AES.encrypt(newUser.id.toString(), process.env.SECRET)
+        const encryptedUserIdString = encryptedUserId.toString()
+        console.log(encryptedUserIdString)
+        // store the encrypted id in the cookie of the res obj
+        res.cookie('userId', encryptedUserIdString)
+        // redirect back to home page
+        res.redirect('/')
+    }
+})
+
 router.get('/login', (req, res) => {
     res.render('./user/login')
+})
+
+router.post('/login', async (req, res) => {
+    const user = await db.user.findOne({where: {email: req.body.email}})
+    if(!user) {
+        console.log('user not found')
+        res.render('users/login.ejs', {error: 'Invalid email/password'})
+    } else if(!bcrypt.compareSync(req.body.password, user.password)) {
+        console.log('Incorrect Password')
+        res.render('users/login.ejs', {error: 'Invalid email/password'})
+    } else {
+        console.log('loggin in the user!')
+        // encrypt the user id via AES
+        const encryptedUserId = cryptojs.AES.encrypt(user.id.toString(), process.env.SECRET)
+        const encryptedUserIdString = encryptedUserId.toString()
+        console.log(encryptedUserIdString)
+        // store the encrypted id in the cookie of the res obj
+        res.cookie('userId', encryptedUserIdString)
+        // redirect back to home page
+        res.redirect('/')
+    }
 })
 
 router.get('/favorites', (req, res) => {
