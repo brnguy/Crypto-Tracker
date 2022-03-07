f4e1010ddba1bbbfb6e7ae291cb5f960c93411aa
brnguy <nguyen.bryan15@gmail.com>
2022-03-07 11:21:30 -0500

    Removed name from cryptocurrency table

--- models/cryptocurrency.js
+++ models/cryptocurrency.js
@@ -15,7 +15,6 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   cryptocurrency.init({
-    name: DataTypes.STRING,
     assetId: DataTypes.STRING,
     userId: DataTypes.INTEGER
   }, {
